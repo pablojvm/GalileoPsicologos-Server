9fb0502a24f2007af91781147b991efa55a8f116
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-12-03 20:11:41 +0100

    ruta post de reserva

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -1,4 +1,24 @@
 const router = require("express").Router();
 const Appointment = require("../models/Appoinment.model");
 
+router.post("/", async (req, res) => {
+  try {
+    const { psychologist, patient, service, date, coment } = req.body;
+
+    const appointment = await Appointment.create({
+      psychologist,
+      patient,
+      service,
+      date,
+      status: "pending",
+      coment,
+    });
+
+    res.status(201).json(appointment);
+  } catch (err) {
+    console.error("Error creando cita:", err);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+});
+
 module.exports = router;
