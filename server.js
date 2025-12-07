d5a643bc66d13cd02e426ade761470bd8a8630db
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-12-07 16:05:41 +0100

    ajustes de cors

--- server.js
+++ server.js
@@ -5,17 +5,31 @@ require("./db");
 
 const app = express();
 
+// Middleware CORS global
 app.use(cors({
   origin: process.env.ORIGIN, // tu frontend
   credentials: true,
 }));
 
+// Permitir preflight (OPTIONS) en todas las rutas
+app.options("*", cors({
+  origin: process.env.ORIGIN,
+  credentials: true,
+}));
+
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Config adicional
 const config = require("./config");
 config(app);
 
+// Rutas
 const indexRouter = require("./routes/index.routes");
 app.use("/api", indexRouter);
 
+// Manejo de errores
 const handleErrors = require("./errors");
 handleErrors(app);
 
