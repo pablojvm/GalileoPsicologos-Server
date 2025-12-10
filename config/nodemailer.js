315f30bf0d4da87ed42f42d4ee8bdad2e1b95ff6
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-12-10 17:54:55 +0100

    brevo ajustes

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,20 +1,9 @@
-const nodemailer = require("nodemailer");
+const SibApiV3Sdk = require("@getbrevo/brevo");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.BREVO_USER, // normalmente es tu email de brevo
-    pass: process.env.BREVO_SMTP_KEY,
-  },
-  connectionTimeout: 20000,
-});
+const brevo = new SibApiV3Sdk.TransactionalEmailsApi();
+brevo.setApiKey(
+  SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
 
-transporter.verify((err, success) => {
-  if (err) console.error("SMTP verify error:", err);
-  else console.log("SMTP ready with Brevo");
-});
-
-module.exports = { transporter };
-
+module.exports = brevo;
--- routes/email.routes.js
+++ routes/email.routes.js
@@ -1,22 +1,21 @@
 const express = require("express");
-const { transporter } = require("../config/nodemailer.js");
-
+const brevo = require("../config/brevo.js");
 const router = express.Router();
 
 router.post("/send-email", async (req, res) => {
   const { to, subject, message } = req.body;
 
   try {
-    await transporter.sendMail({
-      from: `"Tu App" <${process.env.BREVO_USER}>`,  // <- CORREGIDO
-      to,
+    await brevo.sendTransacEmail({
+      sender: { email: process.env.BREVO_USER},
+      to: [{ email: to }],
       subject,
-      html: `<p>${message}</p>`,
+      htmlContent: `<p>${message}</p>`,
     });
 
-    res.json({ success: true, message: "Correo enviado" });
+    res.json({ success: true, message: "Correo enviado con Brevo API" });
   } catch (error) {
-    console.log(error);
+    console.error("Brevo API error:", error);
     res.status(500).json({ success: false, message: "Error enviando email" });
   }
 });
